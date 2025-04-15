ad44179ef17a962a97cb438d2a72cb5876c908c2
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-04-15 14:15:56 +0000

    completed school practices

---
+++ school-8-insert-again.sql
@@ -0,0 +1,11 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Ben",
+    "Block",
+    "benjamin.block@kellogg.northwestern.edu"
+);
+
+-- primary key #1 is not replaced, rather new data starts with primary key 2
